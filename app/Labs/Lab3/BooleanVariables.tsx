8fe3beec8954268e05f32ff7631d8db75aa6c548
SilverWWW <wsilver222@gmail.com>
2025-10-15 16:17:54 -0700

    type error

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -7,7 +7,8 @@ export default function BooleanVariables() {
   const true2 = true1 || false1;
   const true3 = !false2;
   const true4 = numberVariable === 123; // always use === not ==
-  const true5 = floatingPointNumber !== 321.432;
+  const comparisonValue = 100 + 221.432;
+  const true5 = floatingPointNumber !== comparisonValue;
   const false3 = numberVariable < 100;
   return (
     <div id="wd-boolean-variables">
